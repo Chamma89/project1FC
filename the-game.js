56d58a9a6d00815e7c948a09b57fb13f5d53eb73
fouad <Chamma.fouad@gmail.com>
2017-12-15 12:29:11 +1100

    added reset and draw logic

--- the-game.js
+++ the-game.js
@@ -3,57 +3,68 @@
 // textConent is what's inside the taglines ie <p>hello</p> . hello is the textContent. Value is used for number in the taglines.
 // when using querySelectorALL instead of querySelector.. it puts things in an array. Which means you'll need [] to get the elements within. 
 
-
+var game = true;
 var tiles = document.querySelectorAll('td');
 var addToP1 = document.querySelector('.p1Score');
 var addToP2 = document.querySelector('.p2Score');
 var reset = document.querySelector('.button')
-var winner = "";
 var count = 0;
 var result = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+var winner = true;
+var counterp1 = 0;
+var counterp2 = 0;
+var player1Win = document.querySelector('#player1')
+var player2Win = document.querySelector('#player2')
+var box = "";
+var idx = "";
 
 
 
+if (game !== false){
+	
+	function playerTurn (event) {
+		if (winner === true){
+			//convert to Number
+			box = event.target 
 
-function playerTurn (event) {
+			 idx = Number(box.id)
+			 console.log(idx);
+			 
+			// alternate player
+			count += 1
 
-	//convert to Number
-	var box = event.target 
+			if (result[idx] === " " ){
 
-	 var idx = Number(box.id)
-	 console.log(idx);
-	 
-	// alternate player
-	count += 1
+				if (count % 2 !== 0) {
+					// player 1
+					event.target.classList.add('imageX');
+					result[idx] = "x"
+						
 
-	if (result[idx] === " " ){
+				} else {
+					// player 2
+					event.target.classList.add('imageO')
+					result[idx] = "o"
+					
+				}
 
-		if (count % 2 !== 0) {
-			// player 1
-			event.target.classList.add('imageX');
-			result[idx] = "x"
-			
-
-			
-			
-
-		} else {
-			// player 2
-			event.target.classList.add('imageO')
-			result[idx] = "o"
-			
+				return XandO();
+			}
 		}
 
-		return XandO();
-	}
+		else if (winner === false){
+			alert("Game Over. Click RESET to play again")
 
+		}
+	};
 
-};
+}
 
 
 for(var i = 0; i < 9; i++) {
 			
 		tiles[i].addEventListener('click', playerTurn);
+		
 
 }
 
@@ -66,11 +77,11 @@ function XandO () {
 			(result[0] === "x" && result[4] === "x" && result[8] === "x") || (result[2] === "x" && result[5] === "x" && result[8] === "x") ||
 			(result[6] === "x" && result[7] === "x" && result[8] === "x") || (result[3] === "x" && result[4] === "x" && result[5] === "x") || 
 			(result[1] === "x" && result[4] === "x" && result[7] === "x") || (result[2] === "x" && result[4] === "x" && result[6] === "x")){
-				console.log("Player 1 is the winner");
-				winner = "done";
-				var counter = 0;
-				counter += 1;
-				addToP1.textContent = Number(counter);
+				
+				counterp1 += 1;
+				addToP1.textContent = Number(counterp1);
+				winner = false;
+				player1Win.textContent = "Player 1 WINS!"
 
 		
 		}
@@ -79,26 +90,40 @@ function XandO () {
 		 	(result[0] === "o" && result[4] === "o" && result[8] === "o") || (result[2] === "o" && result[5] === "o" && result[8] === "o") ||
 			(result[6] === "o" && result[7] === "o" && result[8] === "o") || (result[3] === "o" && result[4] === "o" && result[5] === "o") ||
 			(result[1] === "o" && result[4] === "o" && result[7] === "o") || (result[2] === "o" && result[4] === "o" && result[6] === "o")){
-				console.log("Player 2 is the winner");
-				winner = "done";
-				var counter = 0;
-				counter += 1;
-				addToP2.textContent = Number(counter);
+			
+				counterp2 += 1;
+				addToP2.textContent = Number(counterp2);
+				winner = false;
+				player2Win.textContent = "Player 2 WINS!"
 
 	}
+
+
+
+	if (!result.includes(" ") ){
+		alert("It's a draw!")
+	}
+	
 	
 }
 
 function resetButton (){
-	if (winner === "done"){
-		event.target.classList.remove('imageO');
-		event.target.classList.remove('imageX');
+		for(var i = 0 ; i < tiles.length; i++){
 
+			tiles[i].classList.remove('imageO');
+			tiles[i].classList.remove('imageX');
+			result = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+
+		}	
+
+		player1Win.textContent = "Player 1 Score"
+		player2Win.textContent = "Player 2 Score"
+		winner = true;
 	}
-}
+		
 
-reset.addEventListener("click", resetButton);
-
-
+reset.addEventListener('click', resetButton);
 
 
+
+	
